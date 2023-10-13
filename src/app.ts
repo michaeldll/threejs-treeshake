83a25ca96425ec777bd8d08dda7da8668381a0fd
Michael de Laborde <ac2031@hotmail.com>
2023-10-13 10:22:10 +0200

    Add three

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import "./scss/global.scss";
+import { BoxGeometry, Mesh, MeshNormalMaterial, PerspectiveCamera, Scene, WebGLRenderer } from "three";
 import { callWhenReady } from "./utils";
 
 type Window = typeof window & {
@@ -7,7 +8,37 @@ type Window = typeof window & {
 
 const init = () => {
   console.log("ready");
-  
+
+  const width = window.innerWidth, height = window.innerHeight;
+
+  // init
+
+  const camera = new PerspectiveCamera(70, width / height, 0.01, 10);
+  camera.position.z = 1;
+
+  const scene = new Scene();
+
+  const geometry = new BoxGeometry(0.2, 0.2, 0.2);
+  const material = new MeshNormalMaterial();
+
+  const mesh = new Mesh(geometry, material);
+  scene.add(mesh);
+
+  const renderer = new WebGLRenderer({ antialias: true });
+  renderer.setSize(width, height);
+  renderer.setAnimationLoop(animation);
+  document.body.appendChild(renderer.domElement);
+
+  // animation
+  function animation(time) {
+
+    mesh.rotation.x = time / 2000;
+    mesh.rotation.y = time / 1000;
+
+    renderer.render(scene, camera);
+
+  }
+
   // Enable hot reloading in development
   if (!(window as Window).IS_PRODUCTION) {
     new EventSource('/esbuild').addEventListener('change', () => location.reload())
